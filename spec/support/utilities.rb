e6820a755d72af1c964b4255098b6d25b3b0c890
riverpig <river.pig.mi@gmail.com>
2014-02-13 16:49:06 +0900

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,3 +12,16 @@ RSpec::Matchers.define :have_error_message do |message|
 	end
 end
 
+def sign_in(user, options={})
+	if options[:no_capybara]
+		#Sign in without capybara
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", with: user.email
+		fill_in "Password", with: user.password
+		click_button "Sign in"
+	end
+end
