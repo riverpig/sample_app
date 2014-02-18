6a136fb4698c49d0fa2a8f0933ab4191b172dd91
riverpig <river.pig.mi@gmail.com>
2014-02-18 11:17:25 +0900

    improve micropost_input_counter.js

--- app/assets/javascripts/micropost_input_counter.js
+++ app/assets/javascripts/micropost_input_counter.js
@@ -1,6 +1,17 @@
 $(function(){
-	$('#micropost_content').bind('keyup',function(){
-		var left_len = 140 - $(this).val().length;
-		$('.count').html(left_len);
+	var div_id = '#micropost_content'
+	displayInputableTextCount(div_id);
+	$(div_id).bind('keyup',function(){
+		displayInputableTextCount(div_id);
 	});
 });
+
+function displayInputableTextCount(div_id){
+	var ret_text;
+	var left_len = 140 - $(div_id).val().length;
+	if(left_len<0)
+		ret_text = "<font color='#ff0000'>" + left_len + "</font>"
+	else
+		ret_text = left_len.toString(10);
+	$('.count').html(ret_text);
+}
